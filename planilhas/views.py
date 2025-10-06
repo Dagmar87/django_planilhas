a3e9fe3a2f5ce7979a39f03dc36d53ddb45a5b15
José Dagmar <dagmar.sobrinho@1telecom.com.br>
2025-10-06 09:34:18 -0300

    Criação da view para importar a planilha

--- planilhas/views.py
+++ planilhas/views.py
@@ -1,3 +1,22 @@
+import pandas as pd
 from django.shortcuts import render
+from .forms import UploadPlanilhaForm
+from .models import Pessoa
 
-# Create your views here.
+def importar_planilha(request):
+    if request.method == 'POST':
+        form = UploadPlanilhaForm(request.POST, request.FILES)
+        if form.is_valid():
+            arquivo = request.FILES['arquivo']
+            df = pd.read_excel(arquivo)
+
+            for _, row in df.iterrows():
+                Pessoa.objects.create(
+                    nome=row['Nome'],
+                    idade=row['Idade'],
+                    cidade=row['Cidade']
+                )
+            return render(request, 'planilhas/sucesso.html')
+        else:
+            form = UploadPlanilhaForm()
+            return render(request, 'planilhas/upload.html', {'form': form})
